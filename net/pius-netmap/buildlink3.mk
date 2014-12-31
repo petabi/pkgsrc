896857ba0d42bc496ba9ef3f33f4b2c2b3ab2058
Min Sik Kim <msk@dolbo.net>
2014-12-31 15:46:46 +0900

    Import pius-netmap-1.6.1

---
+++ net/pius-netmap/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	pius-netmap
+
+.if !defined(PIUS_NETMAP_BUILDLINK3_MK)
+PIUS_NETMAP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.pius-netmap+=	pius-netmap>=1.0
+BUILDLINK_PKGSRCDIR.pius-netmap?=	../../net/pius-netmap
+BUILDLINK_DEPMETHOD.pius-netmap?=	build
+.endif	# PIUS_NETMAP_BUILDLINK3_MK
+
+.include "../../devel/boost-headers/buildlink3.mk"
+
+BUILDLINK_TREE+=	-pius-netmap
