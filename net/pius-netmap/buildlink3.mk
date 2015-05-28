84f12e7c466a1bf89db803426f63312944c003d7
Min Sik Kim <msk@dolbo.net>
2015-05-29 07:20:29 +0900

    Bump version requirement in pius-netmap's buildlink3.mk

--- net/pius-netmap/buildlink3.mk
+++ net/pius-netmap/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	pius-netmap
 .if !defined(PIUS_NETMAP_BUILDLINK3_MK)
 PIUS_NETMAP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.pius-netmap+=	pius-netmap>=1.0
+BUILDLINK_API_DEPENDS.pius-netmap+=	pius-netmap>=1.3
 BUILDLINK_PKGSRCDIR.pius-netmap?=	../../net/pius-netmap
 BUILDLINK_DEPMETHOD.pius-netmap?=	build
 .endif	# PIUS_NETMAP_BUILDLINK3_MK
