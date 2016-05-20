3709a92d39a11a76b302c06fe008f90e3e0c3697
Min Sik Kim <msk@dolbo.net>
2016-05-21 05:09:00 +0900

    Buildlinking rematch requires pcre2

--- devel/rematch/buildlink3.mk
+++ devel/rematch/buildlink3.mk
@@ -9,6 +9,7 @@ BUILDLINK_API_DEPENDS.rematch+=	rematch>=1.3
 BUILDLINK_PKGSRCDIR.rematch?=	../../devel/rematch
 BUILDLINK_DEPMETHOD.rematch?=	build
 
+.include "../../devel/pcre2/buildlink3.mk"
 .endif	# REMATCH_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-rematch
