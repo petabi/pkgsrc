d141e839a95497840e2951761e060f2014cdb1e7
Min Sik Kim <msk@dolbo.net>
2016-12-21 16:04:28 -0800

    Update pius-session to 0.21.2

--- net/pius-session/buildlink3.mk
+++ net/pius-session/buildlink3.mk
@@ -5,10 +5,10 @@ BUILDLINK_TREE+=	pius-session
 .if !defined(PIUS_SESSION_BUILDLINK3_MK)
 PIUS_SESSION_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.pius-session+=	pius-session>=0.14.2
-BUILDLINK_DEPMETHOD.pius-session?=	build
+BUILDLINK_API_DEPENDS.pius-session+=	pius-session>=0.21.0
 BUILDLINK_PKGSRCDIR.pius-session?=	../../net/pius-session
 
+.include "../../net/pius-netmap/buildlink3.mk"
 .endif	# PIUS_SESSION_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-pius-session
