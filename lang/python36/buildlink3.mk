7b17ceec8252a328c81b37ec37a284da8c311761
Min Sik Kim <msk@dolbo.net>
2017-03-18 20:09:22 -0700

    lang/python36: Enable the framework option

--- lang/python36/buildlink3.mk
+++ lang/python36/buildlink3.mk
@@ -12,7 +12,16 @@ BUILDLINK_PKGSRCDIR.python36?=		../../lang/python36
 BUILDLINK_DEPMETHOD.python36?=	${BUILDLINK_DEPMETHOD.python}
 .endif
 
+.include "../../mk/bsd.fast.prefs.mk"
+
+pkgbase := python36
+.include "../../mk/pkg-build-options.mk"
+
+.if !empty(PKG_BUILD_OPTIONS.python36:Mframework)
+BUILDLINK_INCDIRS.python36+=	Library/Frameworks/Python.framework/Versions/3.6/include/python3.6
+.else
 BUILDLINK_INCDIRS.python36+=	include/python3.6
+.endif
 BUILDLINK_LIBDIRS.python36+=	lib/python3.6/config
 BUILDLINK_TRANSFORM+=		l:python:python3.6
 
