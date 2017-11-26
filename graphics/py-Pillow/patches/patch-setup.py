f7a11e59958575dd5cc128a30a115d4b1ad46992
Min Sik Kim <msk@dolbo.net>
2017-11-26 11:11:19 -0800

    Make py-pillow build on macOS

--- graphics/py-Pillow/patches/patch-setup.py
+++ graphics/py-Pillow/patches/patch-setup.py
@@ -3,9 +3,17 @@ $NetBSD: patch-setup.py,v 1.13 2017/07/03 21:51:57 adam Exp $
 Prevent to detect optional tkinter.
 Disable demo programs.
 
---- setup.py.orig	2017-07-01 15:55:53.000000000 +0000
+--- setup.py.orig	2017-10-02 15:20:24.000000000 +0000
 +++ setup.py
-@@ -104,11 +104,7 @@ def get_version():
+@@ -58,7 +58,6 @@ def _dbg(s, tp=None):
+ def _add_directory(path, subdir, where=None):
+     if subdir is None:
+         return
+-    subdir = os.path.realpath(subdir)
+     if os.path.isdir(subdir) and subdir not in path:
+         if where is None:
+             _dbg('Appending path %s', subdir)
+@@ -107,11 +106,7 @@ def get_version():
          exec(compile(f.read(), version_file, 'exec'))
      return locals()['__version__']
  
@@ -18,7 +26,7 @@ Disable demo programs.
  
  NAME = 'Pillow'
  PILLOW_VERSION = get_version()
-@@ -768,7 +764,6 @@ try:
+@@ -779,7 +774,6 @@ try:
            ext_modules=[Extension("PIL._imaging", ["_imaging.c"])],
            include_package_data=True,
            packages=find_packages(),
