bdc5fff7ffa2ed3cc5acec10e62c2c7ec4b086dc
Min Sik Kim <msk@dolbo.net>
2017-12-21 15:21:49 -0800

    Update petabi-ip_tools to 0.3.0

--- net/petabi-ip_tools/buildlink3.mk
+++ net/petabi-ip_tools/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	petabi-ip_tools
 .if !defined(PETABI_IP_TOOLS_BUILDLINK3_MK)
 PETABI_IP_TOOLS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.petabi-ip_tools+=	petabi-ip_tools>=0.1.3
+BUILDLINK_API_DEPENDS.petabi-ip_tools+=	petabi-ip_tools>=0.3.0
 BUILDLINK_PKGSRCDIR.petabi-ip_tools?=	../../net/petabi-ip_tools
 
 .endif	# PETABI_IP_TOOLS_BUILDLINK3_MK
