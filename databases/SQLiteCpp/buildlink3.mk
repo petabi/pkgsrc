54e0ee1b20810d63963ca89e34bc877ead50e5c7
Min Sik Kim <msk@dolbo.net>
2018-01-15 16:56:46 -0800

    Update SQLiteCpp to 2.2.0

---
+++ databases/SQLiteCpp/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	SQLiteCpp
+
+.if !defined(SQLITECPP_BUILDLINK3_MK)
+SQLITECPP_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.SQLiteCpp+=	SQLiteCpp>=2.2.0
+BUILDLINK_DEPMETHOD.SQLiteCpp?=		build
+BUILDLINK_PKGSRCDIR.SQLiteCpp?=		../../databases/SQLiteCpp
+
+.include "../../databases/sqlite3/buildlink3.mk"
+.endif	# SQLITECPP_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-SQLiteCpp
